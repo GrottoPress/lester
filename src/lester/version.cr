00a1d53e2a27bdcd1ccbdffe6b10c48f53bf3e15
akadusei <attakusiadusei@gmail.com>
2025-06-06 12:48:58 +0000

    Fix compile error calling `.compare_versions` with `Lester::VERSION`
    ```
    Error: expected first argument to 'compare_versions' to be a StringLiteral, SymbolLiteral or MacroId, not MacroExpression
    ```

--- src/lester/version.cr
+++ src/lester/version.cr
@@ -1,3 +1,7 @@
 struct Lester
-  VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
+  private macro set_version
+    VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
+  end
+
+  set_version
 end
